4f0b02cfa41862b79d52de9772423ebfbec1bbaf
Lamine Diarra <laminecoder@gmail.com>
2024-11-10 02:07:46 +0000

    Feat: Return notif_token insteadOf pay_token

--- src/Application/OrangeMoneyGateway.php
+++ src/Application/OrangeMoneyGateway.php
@@ -15,40 +15,36 @@ class OrangeMoneyGateway implements OrangeMoneyGatewayInterface
 {
     public static function execute(OrangeMoneyAction $action, ?OrangeMoneyConfig $config = null): OrangeMoneyPaymentResponse
     {
-        try {
-            $accessor = new PropertyAccessor();
-            if (null == $config) {
-                $config = OrangeMoneyConstant::defaultConfig();
-            }
-            $response = OrangeMoneyHttpClient::post($config->getTokenUrl(), [
-                'body'=>array('grant_type' => 'client_credentials'),
-                "headers"=>[
-                    "Authorization: Basic " . $action->getOrangeBearer(),
-                    'Content-Type: application/x-www-form-urlencoded;charset=utf-8'
-                ]
-            ]);
-            $bearerResponse = json_decode($response->getContent(false), true);
-            if ($response->getStatusCode()!==OrangeMoneyConstant::HTTP_OK) {
-                $error = $accessor->getValue($bearerResponse, '[error]');
-                $error_description = $accessor->getValue($bearerResponse, '[error_description]');
-                throw new OrangeMoneyException($error, $error_description);
-            }
-            $response = OrangeMoneyHttpClient::post($config->getProccessUrl(), [
-                'body'=>json_encode($action->getPayload()),
-                "headers"=>[
-                    sprintf("Authorization: %s %s", $bearerResponse['token_type'], $bearerResponse['access_token']),
-                    "Content-Type: application/json"
-                ]
-            ]);
-            $urlResponse = json_decode($response->getContent(false), true);
-            if ($response->getStatusCode()!==OrangeMoneyConstant::HTTP_CREATED) {
-                $message = $accessor->getValue($urlResponse, '[message]');
-                $description = $accessor->getValue($urlResponse, '[description]');
-                throw new OrangeMoneyException($message, $description);
-            }
-            return new OrangeMoneyPaymentResponse($urlResponse['pay_token'], $urlResponse['payment_url']);
-        } catch (\Throwable $th) {
-            throw new OrangeMoneyException($th->getMessage());
+        $accessor = new PropertyAccessor();
+        if (null == $config) {
+            $config = OrangeMoneyConstant::defaultConfig();
         }
+        $response = OrangeMoneyHttpClient::post($config->getTokenUrl(), [
+            'body' => array('grant_type' => 'client_credentials'),
+            "headers" => [
+                "Authorization: Basic " . $action->getOrangeBearer(),
+                'Content-Type: application/x-www-form-urlencoded;charset=utf-8'
+            ]
+        ]);
+        $bearerResponse = json_decode($response->getContent(false), true);
+        if ($response->getStatusCode() !== OrangeMoneyConstant::HTTP_OK) {
+            $error = $accessor->getValue($bearerResponse, '[error]');
+            $error_description = $accessor->getValue($bearerResponse, '[error_description]');
+            throw new OrangeMoneyException($error, $error_description);
+        }
+        $response = OrangeMoneyHttpClient::post($config->getProccessUrl(), [
+            'body' => json_encode($action->getPayload()),
+            "headers" => [
+                sprintf("Authorization: %s %s", $bearerResponse['token_type'], $bearerResponse['access_token']),
+                "Content-Type: application/json"
+            ]
+        ]);
+        $urlResponse = json_decode($response->getContent(false), true);
+        if ($response->getStatusCode() !== OrangeMoneyConstant::HTTP_CREATED) {
+            $message = $accessor->getValue($urlResponse, '[message]');
+            $description = $accessor->getValue($urlResponse, '[description]');
+            throw new OrangeMoneyException($message, $description);
+        }
+        return new OrangeMoneyPaymentResponse($urlResponse['notif_token'], $urlResponse['payment_url']);
     }
 }
